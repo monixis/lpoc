8d132ecba70f5454767961893d990069d6e385c8
Deep Dand <deep.dand1992@gmail.com>
2018-08-10 15:45:53 -0400

    start time sequence changed

--- application/views/reviewRequest.php
+++ application/views/reviewRequest.php
@@ -317,8 +317,9 @@
                     <label class="label">Event Name:</label><br/><input type="text" id="eventName" class="textinput" value = "<?php echo $eventName; ?>" readonly />
           			<label class="label">Event Description:</label><br/><textarea id="eventDesc" class="readonlytext" style="height: 150px; overflow: auto; width: 400px;" readonly><?php echo $eventDesc; ?></textarea>
                     <label class="label">Start Date:</label><br/><input type="text" id="startdatepicker" class="textinput"  value = "<?php echo $eventStartDate; ?>" style="width: 100px;"readonly/>
-                    <label class="label">End Date:</label><br/><input type="text" id="enddatepicker" class="textinput"  value = "<?php echo $eventEndDate; ?>" style="width: 100px;"readonly/>
                     <label class="label">Start Time:</label><br/><input type="time" id="startTime" class="textinput"  value = "<?php echo $startTime; ?>" style="width: 100px;"readonly/>
+                    <label class="label">End Date:</label><br/><input type="text" id="enddatepicker" class="textinput"  value = "<?php echo $eventEndDate; ?>" style="width: 100px;"readonly/>
+                    
                     <label class="label">End Time:</label><br/><input type="time" id="endTime" class="textinput"  value = "<?php echo $endTime; ?>" style="width: 100px;"readonly/>
                     <!--label class="label">Event Type:</label><br/--><input type="hidden" id="eventType" class="textinput" value = "<?php echo $eventType; ?>" readonly/>
                     <!--label class="label">Room Id:</label><br/--><input type="hidden" id="roomId" class="textinput" value = "<?php echo $roomId; ?>" readonly/>
